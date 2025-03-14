8a6e343fa232cbea916bd607946ec2b71b484062
David Megli <verbatimdvd91@gmail.com>
2025-03-14 15:24:00 +0100

    implemented evaluation

--- scripts/evaluate.py
+++ scripts/evaluate.py
@@ -9,6 +9,7 @@ from torch import nn
 from src.dataset import get_mnist_dataloaders
 from src.model import MLP
 import src.config as config
+from src.train_utils import evaluate
 import numpy as np
 
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
@@ -18,3 +19,5 @@ model.eval()
 
 train_dataloader, test_dataloader, val_dataloader = get_mnist_dataloaders(config.BATCH_SIZE, config.NUM_WORKERS)
 
+test_loss, test_accuracy = evaluate(model, test_dataloader, device)
+print(f"Test Loss: {test_loss:.4f}, Test Accuracy: {test_accuracy:.4f}")
